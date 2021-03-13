2a1352dc16b652a19a0b8e2da63e0a9bbb665f99
Dmytro Shults <dmytroshults@gmail.com>
2021-03-13 19:18:09 +0100

    Add command to delete all users except admin

---
+++ mysite/members/management/commands/reset_users.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    help = "Delets all users except admin."
+    queryset = User.objects.all()
+    keep = 'admin'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--users', nargs='+',
+            help='Specify users you want to keep.',
+        )
+
+    def handle(self, users, *args, **options):
+        queryset = self.queryset.exclude(username=self.keep)
+        if users:
+            for user in users:
+                queryset = queryset.exclude(username=user)
+            users.append(self.keep)
+        else:
+            users = self.keep
+        queryset.delete()
+        self.stdout.write(self.style.SUCCESS(
+            f"Successfully deleted users: {users}"))
