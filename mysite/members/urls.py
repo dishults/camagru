f1fca7cac33d1c6ae0498ec8d90cae145167ba66
Dmytro Shults <dmytroshults@gmail.com>
2021-03-13 17:08:38 +0100

    Add logout, improve views

--- mysite/members/urls.py
+++ mysite/members/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('login', views.LoginView.as_view(), name='login'),
+    path('logout', views.logout_view, name='logout'),
     path('signup', views.SignupView.as_view(), name='signup'),
 ]
--- mysite/members/views.py
+++ mysite/members/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import login, logout
-from django.views.generic import FormView
+from django.views.generic import FormView, TemplateView
+from django.shortcuts import redirect
 
 from members.forms import LoginForm, SignupForm
 
@@ -9,19 +10,34 @@ class LoginView(FormView):
     template_name = 'members/login.html'
     context_object_name = 'members_login_view'
     form_class = LoginForm
-    success_url = '/login'
+    success_url = '/login'  # galery
 
     def form_valid(self, form):
-        login(self.request, form.user_cache)
-        messages.success(self.request, "You've logged in successfully.")
+        if self.request.user == form.user_cache:
+            messages.warning(self.request, "You've already logged in.")
+        else:
+            login(self.request, form.user_cache)
+            messages.success(self.request, "You've logged in successfully.")
         return super().form_valid(form)
+
+
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request, "You've been logged out.")
+    return redirect(LoginView.success_url)
 
 
 class SignupView(FormView):
     template_name = 'members/signup.html'
     context_object_name = 'members_signup_view'
     form_class = SignupForm
-    success_url = '/signup'
+    success_url = '/signup'  # galery
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(LoginView.success_url)
+        return super().get(request, *args, **kwargs)
 
     def form_valid(self, form):
         user = form.save()
