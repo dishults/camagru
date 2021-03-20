517e4b78ab98bee44900aa116ac4668ada9f5867
Dmytro Shults <dmytroshults@gmail.com>
2021-03-20 17:17:26 +0100

    Create global templates and static folders + improve buttons

--- mysite/gallery/urls.py
+++ mysite/gallery/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.GalleryView.as_view(), name='gallery'),
+    path('', views.GalleryView.as_view(), name='homepage'),
     path('images/<image>', views.show_image, name='show-image'),
 ]
