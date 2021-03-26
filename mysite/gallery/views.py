83ef4e28862d996dea963fb8facc9d16c9f627e9
Dmytro Shults <dmytroshults@gmail.com>
2021-03-26 17:59:33 +0100

    Add user notification on comment

--- mysite/gallery/views.py
+++ mysite/gallery/views.py
@@ -1,7 +1,12 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.mail import EmailMessage
 from django.views.generic import ListView, FormView
-from .models import Image, Like
 
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+from .models import Image, Like
 from .forms import CommentForm
 
 
@@ -13,9 +18,9 @@ class GalleryView(ListView, FormView):
     success_url = '/'
 
     def form_valid(self, form):
+        image = form.cleaned_data.get('image')
         button = self.request.POST.get('button')
         if button == 'like':
-            image = form.cleaned_data.get('image')
             _, created = Like.objects.get_or_create(
                 image=image, user=self.request.user)
             if not created:
@@ -28,4 +33,14 @@ class GalleryView(ListView, FormView):
             saved = form.save(commit=False)
             saved.user = self.request.user
             saved.save()
+            if image and image.user != self.request.user and image.user.email:
+                EmailMessage(
+                    subject='New picture comment',
+                    body=render_to_string('gallery/notify.html', {
+                        'image': image,
+                        'from_user': self.request.user,
+                        'domain': get_current_site(self.request).domain,
+                    }),
+                    to=[image.user.email]
+                ).send()
         return super().form_valid(form)
