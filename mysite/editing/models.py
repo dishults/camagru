271ea1032bca47b55be4298d39495059b4a49122
Dmytro Shults <dmytroshults@gmail.com>
2021-03-27 18:10:22 +0100

    Create Overlay model

--- mysite/editing/models.py
+++ mysite/editing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from gallery.validators import validate_size
+
+
+class Overlay(models.Model):
+
+    image = models.ImageField(
+        upload_to='static/overlays', validators=[validate_size])
+
+    def __str__(self):
+        return self.image.name
