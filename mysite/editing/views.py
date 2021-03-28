4bd5a06e0ee02c3baa50b59aebf83acb06c97634
Dmytro Shults <dmytroshults@gmail.com>
2021-03-28 12:05:17 +0200

    Shod overlays and thumbnails

--- mysite/editing/views.py
+++ mysite/editing/views.py
@@ -1,12 +1,24 @@
 from django.views.generic import View
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from gallery.models import Image
+from .models import Overlay
 
 
 class EditingView(View):
     template_name = 'editing/editing.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        images = Image.objects.filter(
+            user=request.user).order_by('-date_created')
+        overlays = Overlay.objects.all()
+        context = {
+            'images': images,
+            'overlays': overlays,
+        }
+
+        return render(request, self.template_name, context)
 
     def post(self, request):
         return super().post(request)
