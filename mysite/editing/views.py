3c649bdc2089be85488b2381b72f519193f761d2
Dmytro Shults <dmytroshults@gmail.com>
2021-04-02 14:26:41 +0200

    Refactor to get user resources more efficiently

--- mysite/editing/views.py
+++ mysite/editing/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import View
 from django.shortcuts import redirect, render
 
-from gallery.models import Image
 from .models import Overlay
 
 
@@ -9,8 +8,7 @@ class EditingView(View):
     template_name = 'editing/editing.html'
 
     def get(self, request):
-        images = Image.objects.filter(
-            user=request.user).order_by('-date_created')
+        images = request.user.image_set.all().order_by('-date_created')
         overlays = Overlay.objects.all()
         context = {
             'images': images,
@@ -25,6 +23,6 @@ class EditingView(View):
 
 def delete_image(request, image_id):
     # To make sure that static files will also be removed
-    for image in Image.objects.filter(user=request.user, id=image_id):
+    for image in request.user.image_set.filter(id=image_id):
         image.delete()
     return redirect('editing')
