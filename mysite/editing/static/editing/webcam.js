d5fd431ec39094e89e5ee320b8b2723ccca17e09
Dmytro Shults <dmytroshults@gmail.com>
2021-04-05 17:28:26 +0200

    Add possibility to apply overlays and load pics from thumbs

--- mysite/editing/static/editing/webcam.js
+++ mysite/editing/static/editing/webcam.js
@@ -1,19 +1,21 @@
 (function () {
 
-    var width = 320;    // We will scale the photo width to this
+    var width = 500;    // We will scale the photo width to this
     var height = width / (4 / 3);     // This will be computed based on the input stream
 
     var video = document.getElementById("video");
     var canvas = document.getElementById('canvas');
     var preview = canvas.getContext('2d');
 
+    var overlays = document.getElementsByName('overlays');
+    var thumbnails = document.getElementsByName('thumbnails');
     var snapshot = document.getElementById('snapshot');
     var upload = document.getElementById('id_image');
     var imageString = document.getElementById('id_image_string');
 
     if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-            .then(function (stream) {
+            .then(stream => {
                 video.srcObject = stream;
                 height = video.videoHeight / (video.videoWidth / width);
 
@@ -24,7 +26,7 @@
                 video.setAttribute('width', width);
                 video.setAttribute('height', height);
             })
-            .catch(function (err) {
+            .catch(err => {
                 document.getElementById('webcam').hidden = true;
                 console.log("Couldn't get a stream: " + err);
             });
@@ -32,7 +34,7 @@
         canvas.setAttribute('height', height);
     }
 
-    snapshot.addEventListener('click', function (e) {
+    snapshot.addEventListener('click', () => {
         upload.value = '';
         preview.clearRect(0, 0, canvas.width, canvas.height);
         preview.drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -42,6 +44,7 @@
 
     upload.addEventListener('change', function (e) {
         if (this.files && this.files[0]) {
+            imageString.value = '';
             var image = new Image();
             image.onload = function () {
                 height = image.height / (image.width / width);
@@ -51,6 +54,30 @@
             image.src = URL.createObjectURL(this.files[0]);;
             URL.revokeObjectURL(image.src);
         }
+    });
+
+    function applyOverlay() {
+        const image = this.children[0];
+        preview.drawImage(image, 0, 0, width, height);
+    }
+
+    overlays.forEach(item => {
+        item.addEventListener('click', applyOverlay);
+    });
+
+    function loadImage() {
+        upload.value = '';
+        const image = this.children[0];
+        const imageElem = new Image();
+        imageElem.src = '/static/images/' + image.src.split('/').slice(-1)[0];
+
+        height = imageElem.height / (imageElem.width / width);
+        preview.clearRect(0, 0, canvas.width, canvas.height);
+        preview.drawImage(imageElem, 0, 0, width, height);
+    }
+
+    thumbnails.forEach(item => {
+        item.addEventListener('click', loadImage);
     });
 
 })();
