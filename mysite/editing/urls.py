c891bbf89f2b3f4e968b906ac738e355d4b0baf5
Dmytro Shults <dmytroshults@gmail.com>
2021-04-24 11:18:42 +0200

    Change urls to include a trailing slash

--- mysite/editing/urls.py
+++ mysite/editing/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
+    # Has to be without a trailing slash so that static thumbs and overlays load properly
     path('editing', login_required(views.EditingView.as_view()), name='editing'),
-    path('delete/<int:image_id>', login_required(views.delete_image),
+    path('editing/', RedirectView.as_view(pattern_name='editing', permanent=True)),
+    path('delete/<int:image_id>/', login_required(views.delete_image),
          name='delete_image'),
 ]
--- mysite/members/urls.py
+++ mysite/members/urls.py
@@ -5,15 +5,15 @@ from . import views
 
 urlpatterns = [
     path('accounts/login/', views.SigninView.as_view(), name='login'),
-    path('signin', views.SigninView.as_view(), name='signin'),
-    path('signout', views.signout_view, name='signout'),
-    path('signup', views.SignupView.as_view(), name='signup'),
+    path('signin/', views.SigninView.as_view(), name='signin'),
+    path('signout/', views.signout_view, name='signout'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
-    path('settings', login_required(views.SettingsView.as_view()), name='settings'),
+    path('settings/', login_required(views.SettingsView.as_view()), name='settings'),
 
-    path('password_reset', views.PasswordResetCustomView.as_view(),
+    path('password_reset/', views.PasswordResetCustomView.as_view(),
          name='password_reset'),
-    path('password_reset/done', views.PasswordResetCustomDoneView.as_view(),
+    path('password_reset/done/', views.PasswordResetCustomDoneView.as_view(),
          name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.PasswordResetCustomConfirmView.as_view(),
          name='password_reset_confirm'),
